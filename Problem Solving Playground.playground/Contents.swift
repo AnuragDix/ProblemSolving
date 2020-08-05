963a04498f08ae0afb116baa248b9a464faa2d33
anuragdixitgwl <anuragdixitgwl@gmail.com>
2020-08-05 12:43:09 +0530

    Duplicate arraY PROBLEM

--- Problem Solving Playground.playground/Contents.swift
+++ Problem Solving Playground.playground/Contents.swift
@@ -9,6 +9,32 @@ var y = 10
 x = y - x
 y = x
 x = x + y
+//print (x,y)
 
 
-print (x,y)
+//swap two numbers without third variable with XOR
+var xx = 5
+var yy = 10
+
+xx = xx ^ yy
+yy = xx ^ yy
+xx = xx ^ yy
+print (xx,yy)
+
+
+//Remove duplicate from sorted array
+var array = [1,2,2,3,3,4,4,5]
+
+var j : Int = 0
+for i in 0...array.count where i < array.count - 1{
+    if array[i] != array[i+1]{
+        array[j] = array[i]
+        j = j + 1
+    }
+}
+array[j] = array[array.count - 1]
+array.removeSubrange(Range<Int>(uncheckedBounds: (lower: array[j + 1], upper: array[array.count - 1])))
+
+print (array)
+
+
